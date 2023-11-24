4caa439d4d483a9b5ea608ed42c0afa522be951b
Renan Matos <renanjs_matos@hotmail.com>
2023-11-24 00:02:20 -0300

    feat(controller/usuarios): adiciona criação de usuários

---
+++ src/controladores/usuarios.js
@@ -0,0 +1,88 @@
+const jwt = require("jsonwebtoken")
+const knex = require("../conexao")
+const bcrypt = require("bcrypt")
+const senhajwt = require("../senhajwt")
+
+const cadastrarUsuario = async (req , res) => {
+    const {nome, email , senha , telefones} = req.body
+
+    console.log(nome , email, senha, telefones)
+
+    
+    if(!nome){
+        return res.status(404)
+        .json({mensagem: "É necessario preencher todos os campos!"})
+    }
+
+    if(!email){
+        return res.status(404)
+        .json({mensagem: "É necessario preencher todos os campos!"})
+    }
+
+    if(!senha){
+        return res.status(404)
+        .json({mensagem: "É necessario preencher todos os campos!"})
+    }
+
+    if(!telefones){
+        return res.status(404)
+        .json({mensagem: "É necessario preencher todos os campos!"})
+    }
+
+    try {
+
+       const emailExiste = await knex('usuarios').where({ email }).first()
+
+       if (emailExiste) {
+        return res.status(400).json('O email já existe')
+        }
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10)
+
+        const numerofinal = telefones.map((item) => {
+            return `${item.ddd}${item.numero}`
+        } ).join(";")
+    
+
+		let user = await knex('usuarios')
+			.insert({
+				nome,
+				email,
+				senha: senhaCriptografada,
+				telefones: numerofinal,
+			})
+            .returning("*")
+
+        user = user[0]
+
+        const token = jwt.sign({id: user.id}, senhajwt, {expiresIn: "30m"})
+
+        if(!user){
+            return res.status(400).json({mensagem: "O usuario não foi cadastrado."})
+        }
+  
+        const bodyresposta = {
+            id: user.id,
+            data_criacao: user.data_criacao,
+            data_atualizacao: user.data_atualizacao,
+            ultimo_login: user.ultimo_login || null,
+            token,
+        }
+        
+        console.log(bodyresposta)
+
+        return res.status(201).json(bodyresposta)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({mensagem: "Erro interno do servidor"})
+    }
+}
+
+
+
+
+
+module.exports = {
+    cadastrarUsuario,
+    
+}
